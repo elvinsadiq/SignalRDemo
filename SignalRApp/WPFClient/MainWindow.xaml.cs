5cc0dd1e2c4d0decb31f52e8e5c0b89843bd472a
elvinsadiq <74909791+elvinsadiq@users.noreply.github.com>
2024-01-17 11:16:36 +0400

    Added WPFClient

---
+++ SignalRApp/WPFClient/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace WPFClient
+{
+    public partial class MainWindow : Window
+    {
+        HubConnection connection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7239/chathub")
+                .WithAutomaticReconnect()
+                .Build();
+
+            connection.Reconnecting += (sender) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMessage = "Attempting to reconnect ...";
+                    messages.Items.Add(newMessage);
+                });
+
+                return Task.CompletedTask;
+            };
+        }
+    }
+}
